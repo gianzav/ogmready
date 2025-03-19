e6f6dbd6503c38ebebe1ae806218e637ced1752a
gianzav <gianlucagianluca18@gmail.com>
2025-03-19 09:50:20 +0100

    chore: Updated gitignore, moved examples.py in its own directory

--- src/examples/examples.py
+++ src/examples/examples.py
@@ -1,8 +1,7 @@
 from typing import Set
-from ogmready import *
+from ogmready.ogmready import *
 from dataclasses import dataclass
 import owlready2
-
 
 
 # ontology
@@ -11,6 +10,7 @@ onto = owlready2.get_ontology("http://example.org/")
 other_namespace = "http://other.org/"
 
 with onto:
+
     class Person(owlready2.Thing):
         pass
 
@@ -30,17 +30,22 @@ with onto:
         domain = [Person]
         range = [Dog]
 
+
 with onto.get_namespace(other_namespace):
+
     class color(owlready2.DataProperty):
         range = [str]
 
+
 # domain classes
+
 
 @dataclass
 class Dog:
     id: int
     name: str
     colors: Set[str]
+
 
 @dataclass
 class Person:
@@ -55,19 +60,21 @@ class DogMapper(Mapper):
         mappings = {
             "id": DataPropertyMapping("id", primary_key=True),
             "name": DataPropertyMapping("name"),
-            "colors": DataPropertyMapping(("color", other_namespace), functional=False)
+            "colors": DataPropertyMapping(("color", other_namespace), functional=False),
         }
         super().__init__(Dog, "Dog", mappings, ontology)
+
 
 class PersonMapper(Mapper):
     def __init__(self, ontology):
         mappings = {
-            "id" : DataPropertyMapping("id", primary_key=True),
+            "id": DataPropertyMapping("id", primary_key=True),
             "name": DataPropertyMapping("name"),
             "age": DataPropertyMapping("age"),
-            "dog": ObjectPropertyMapping("hasDog", lambda: DogMapper(ontology))
+            "dog": ObjectPropertyMapping("hasDog", lambda: DogMapper(ontology)),
         }
         super().__init__(Person, "Person", mappings, ontology)
+
 
 d = Dog(1, "pluto", {"black", "white"})
 p = Person(2, "mario", 10, d)
